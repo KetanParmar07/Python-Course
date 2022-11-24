7ca546c23f530e093c0801fef67e567090950869
Ketan Parmar <kp3849668@gmail.com>
2022-11-24 14:07:51 +0530

    first and second commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# # This is a sample Python script.
+#
+# # Press Shift+F10 to execute it or replace it with your code.
+# # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#
+#
+# def print_hi(name):
+#     # Use a breakpoint in the code line below to debug your script.
+#     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#
+#
+# # Press the green button in the gutter to run the script.
+# if __name__ == '__main__':
+#     print_hi('PyCharm')
+#
+# # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+print('Hello World',' My name is Ketan', (20+30))
+print('My Name is ketan', 'My Age is = ',(12+7),' My Birth Date is= ',(12+15),'/',(5+2),'/',(2000+3),'.')
