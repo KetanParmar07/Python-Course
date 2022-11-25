0b3b9153809fea5ff2f8b5a5b529a6a7de223c8e
Ketan Parmar <kp3849668@gmail.com>
2022-11-25 19:24:04 +0530

    Operator file commit

---
+++ Operator_In_Python/Logical_Operator.py
@@ -0,0 +1,10 @@
+x=54
+y=7
+
+# here (and) Logical operator will check all conditions are True if yes then print statement print the True
+print(x==y and x>y)
+# here (or) Logical operator will check ethier one condition is True if yes then print statement print the True
+print(x==y or x>y)
+# here (not) Logical operator will print reverse value of given value
+print(not x==y)
+print(x==y and x>y or x!=y)
