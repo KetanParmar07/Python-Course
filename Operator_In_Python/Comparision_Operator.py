e4cb667ffc0b91e23f5a866b4f65dbe9cf6f7bdc
Ketan Parmar <kp3849668@gmail.com>
2022-11-25 19:12:21 +0530

    Operator file commit

---
+++ Operator_In_Python/Comparision_Operator.py
@@ -0,0 +1,15 @@
+num1 = float(input('Enter Number1: '))
+num2 = float(input('Enter Number2: '))
+
+# here == operator check the comparision of two value is true or flase
+print(num1 == num2)
+# here != operator check the comparision of two value is true or flase
+print(num1 != num2)
+# here > operator check if num1 value is greater then num2 value or not
+print(num1 > num2)
+# here < operator check if num1 value is less then num2 value or not
+print(num1 < num2)
+# here > operator check if num1 value is greater then or equal to num2 value or not
+print(num1 >= num2)
+# here < operator check if num1 value is less then or equal to num2 value or not
+print(num1 <= num2)
