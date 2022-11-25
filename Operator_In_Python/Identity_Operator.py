7f75ea1311be635f20e935a51bb72f9e276be8af
Ketan Parmar <kp3849668@gmail.com>
2022-11-25 21:17:54 +0530

    Operator file commit

---
+++ Operator_In_Python/Identity_Operator.py
@@ -0,0 +1,7 @@
+x=45
+y=54
+
+# here (is) operator check the comparision of two value is true or flase
+print(x is y)
+# here (is not) operator check the comparision of two value is true or flase
+print(x is not y)
