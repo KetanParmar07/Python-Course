a75cbce28a1cde5ff5b1ec9f0133cd323dc5c116
Ketan Parmar <kp3849668@gmail.com>
2022-11-25 22:09:42 +0530

    Data-Type file commit

---
+++ Data-Type/Data_Type.py
@@ -0,0 +1,10 @@
+x = 10
+y = 7.333
+z = 1+2j
+
+# int
+print(x,' is Data-Type equal to ',type(x))
+# float
+print(y,' is Data-Type equal to ',type(y))
+# complex number
+print(z,' is Data-Type equal to', type(z))
