4f5a6bc3a4b589a1b03109c86535a1f1572dcfb1
Ketan Parmar <kp3849668@gmail.com>
2022-11-25 19:32:36 +0530

    Operator file commit

---
+++ Operator_In_Python/Membership_Operator.py
@@ -0,0 +1,6 @@
+str = 'ketan'
+
+# here (in) operator check given value or string or character is present in the particular data type in or not
+print('k' in str)
+# here (not in) operator check given value or string or character is not present in the particular data type in or not
+print('l' not in str)
