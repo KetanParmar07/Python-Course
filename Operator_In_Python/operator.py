8c3504d5319a728593447ddd58a79bea3d066975
Ketan Parmar <kp3849668@gmail.com>
2022-11-25 18:46:25 +0530

    Operator file commit

---
+++ Operator_In_Python/operator.py
@@ -0,0 +1,17 @@
+num1 = float(input('Enter Number1: '))
+num2 = float(input('Enter Number2: '))
+
+# Addition
+print('The Sum of Two Number is= ',num1+num2)
+# Subtraction
+print('The Subtraction of Two Number is= ',num1-num2)
+# Multiplication
+print('The Multiplication of Two Number is= ',num1*num2)
+# Division
+print('The Division of Two Number is= ',num1/num2)
+# Modulas
+print('The Modulas is= ',num1%num2)
+# Exponent of this given number
+print('The Exponent of this Number is= ',num1**3)
+# Floor value of this division operation in
+print('The Floor value is= ',num1//num2)
