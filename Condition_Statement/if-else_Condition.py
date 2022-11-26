405901eec40c8a57b2b1e1340ef6fafa629c7513
Ketan Parmar <kp3849668@gmail.com>
2022-11-26 13:50:50 +0530

    Data-Type file commit

---
+++ Condition_Statement/if-else_Condition.py
@@ -0,0 +1,12 @@
+num = int(input('Enter a value:- '))
+
+# if statement
+# if statement syntax:-
+# if condition(True):
+        # body
+# else:
+        # body
+if num % 2 == 0:
+    print('Even Number.')
+else:
+    print('Odd Number')
