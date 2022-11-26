2c190d114030a9d939dfc1e9879197f72c0e4dc2
Ketan Parmar <kp3849668@gmail.com>
2022-11-26 07:33:30 +0530

    Data-Type file commit

--- Data-Type/Data_Type.py
+++ Data-Type/Data_Type.py
@@ -8,3 +8,18 @@ print(x,' is Data-Type equal to ',type(x))
 print(y,' is Data-Type equal to ',type(y))
 # complex number
 print(z,' is Data-Type equal to', type(z))
+# string
+s = 'ketan'
+print(s,type(s))
+s = '''
+        Good, Morning
+        Hello, My name is ketan
+        what are doing?
+    '''
+print(s,type(s))
+
+# List Data-Type
+l = [1,'ketan',4.768786,[2,46,75],(55,433,1+3j)]
+print(l,type(l))
+l[3][0] = [11,22,33,44]
+print(l,type(l))
