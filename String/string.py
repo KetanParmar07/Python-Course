3c83e0aed140262d1ee0fb389cc2262917ac72f0
Ketan Parmar <kp3849668@gmail.com>
2022-11-27 18:59:02 +0530

    String file commit

---
+++ String/string.py
@@ -0,0 +1,22 @@
+# string = string is a collection of character
+# string represents in the single, double quotation symbol
+# string is also represent in the triple quotation symbol
+# every string character access using index value
+# index value always start with 0 for left to right iteration and -1 for right to left iteration
+
+# string example given below
+string = 'Hello World'
+string1 = "My name is  Ketan Parmar"
+string2 = '''
+            Good, Morning
+            Hello, My name is ketan
+            what are doing?
+'''
+print(string)
+print(string1)
+print(string2)
+
+print(string[4])
+print(string1[3])
+print(string2[50])
+
