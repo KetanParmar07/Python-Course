3f9579455328639af1cf6a389c0ecbc09c534a92
Ketan Parmar <kp3849668@gmail.com>
2022-11-27 17:43:53 +0530

    Loop file commit

--- Loops/for-Loop.py
+++ Loops/for-Loop.py
@@ -9,3 +9,10 @@
 # here for loop start value 1
 # for loop goes to 5-1 = 4
 # and increment by always 1
+
+# range(1,5,2)
+# here one parameter using for loop first value for start condition and second value for condition check and third value for incerment or decrement value
+# here for loop start value 1
+# for loop goes to 5-1 = 4
+# and increment by always 2
+
