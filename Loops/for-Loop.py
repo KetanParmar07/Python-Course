23b0d4e25ebf8b21f2721db3de3eeac3f88b6b0d
Ketan Parmar <kp3849668@gmail.com>
2022-11-27 17:41:05 +0530

    Loop file commit

--- Loops/for-Loop.py
+++ Loops/for-Loop.py
@@ -3,3 +3,9 @@
 # here for loop start value always 0
 # for loop goes to 5-1 = 4
 # and increment by always 1
+
+# range(1,5)
+# here two parameter using for loop first value for start condition and second value for condition check
+# here for loop start value 1
+# for loop goes to 5-1 = 4
+# and increment by always 1
