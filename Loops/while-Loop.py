ab9d76ac1243ad85e657cadd1b33fe616908057b
Ketan Parmar <kp3849668@gmail.com>
2022-11-27 18:19:46 +0530

    Loop file commit

---
+++ Loops/while-Loop.py
@@ -0,0 +1,19 @@
+# start
+# condition
+# increment/decrement
+
+# Example of while loop
+print('The number from 1 to 10: ')
+i = 1
+while i <= 10:
+    print(i)
+    i+=1
+
+print('------------------------------------------')
+
+# Example of multiplication table
+print('Multiplication Table of 3 is = ')
+i = 1
+while i <= 10:
+    print('3',' x ',i,' = ',3*i)
+    i+=1
